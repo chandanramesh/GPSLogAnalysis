dfcd191485c0353357ae99d2c2e7f82ef153dc79
chandanramesh <srchandan.in@gmail.com>
2016-06-07 07:26:42 +0530

    Action Bar addition

--- app/src/main/java/com/irail/chandansr/gpsloganalysis/MainActivity.java
+++ app/src/main/java/com/irail/chandansr/gpsloganalysis/MainActivity.java
@@ -17,6 +17,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -235,5 +237,21 @@ public class MainActivity extends AppCompatActivity  implements LocationListener
         Log.d(TAG ,"Launching new activity to process Logs");
         Intent intent = new Intent(this,LogProcessAcitivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.actionbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.suplact:
+                break;
+        }
+        return true;
     }
 }
