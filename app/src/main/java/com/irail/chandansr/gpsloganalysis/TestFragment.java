8f5c0ccf82393f4505fe486f424882e6846a58d6
chandanramesh <discover.135>
2016-06-12 20:43:09 +0530

    Textview Fragment , which wont work.

--- app/src/main/java/com/irail/chandansr/gpsloganalysis/TestFragment.java
+++ app/src/main/java/com/irail/chandansr/gpsloganalysis/TestFragment.java
@@ -1,13 +1,22 @@
 package com.irail.chandansr.gpsloganalysis;
 
 import android.app.Fragment;
+import android.graphics.Color;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.Iterator;
 
 /**
  * Created by chandan.sr on 07-Jun-16.
@@ -15,6 +24,8 @@ import android.view.ViewGroup;
 
 public class TestFragment extends Fragment {
     public final String TAG = "GPS Log Analyis";
+    GpsStatusService mGpsStatusService;
+    static View v;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +35,10 @@ public class TestFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.test_setting_fragment,container,false);
+        v = inflater.inflate(R.layout.test_setting_fragment,container,false);
         return v;
     }
+
+
 
 }
